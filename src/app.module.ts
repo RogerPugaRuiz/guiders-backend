9fe8c9fd431839486aef2aea0d22d65dc6339742
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-04-03 11:51:56 +0200

    fix(database-config): renombrar variable DATABASE_DATABASE a DATABASE en docker-compose y app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -44,7 +44,7 @@ import { TokenVerifyService } from './context/shared/infrastructure/token-verify
       port: parseInt(process.env.DATABASE_PORT!, 10) || 5432,
       username: process.env.DATABASE_USERNAME || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'password',
-      database: process.env.DATABASE_DATABASE || 'mydb',
+      database: process.env.DATABASE || 'mydb',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // Solo para desarrollo
     }),
@@ -66,15 +66,15 @@ export class AppModule {
       this.configService.get<string>('DATABASE_USERNAME');
     const DATABASE_PASSWORD =
       this.configService.get<string>('DATABASE_PASSWORD');
-    const DATABASE_DATABASE =
-      this.configService.get<string>('DATABASE_DATABASE');
+    const DATABASE =
+      this.configService.get<string>('DATABASE');
     this.logger.log(`ENCRYPTION_KEY: ${ENCRYPTION_KEY}`);
     this.logger.log(`GLOBAL_TOKEN_SECRET: ${GLOBAL_TOKEN_SECRET}`);
     this.logger.log(`DATABASE_HOST: ${DATABASE_HOST}`);
     this.logger.log(`DATABASE_PORT: ${DATABASE_PORT}`);
     this.logger.log(`DATABASE_USERNAME: ${DATABASE_USERNAME}`);
     this.logger.log(`DATABASE_PASSWORD: ${DATABASE_PASSWORD}`);
-    this.logger.log(`DATABASE_DATABASE: ${DATABASE_DATABASE}`);
+    this.logger.log(`DATABASE: ${DATABASE}`);
 
     this.logger.log(`ENCRYPTION_KEY: ${process.env.ENCRYPTION_KEY}`);
   }
