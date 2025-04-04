b82f244b0e7ba7aaca436ec82062699ea152a68e
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-04-04 10:54:29 +0200

    fix(encrypt-adapter): mejorar el registro de datos encriptados con formato más claro

--- src/context/auth-context/api-key/infrastructure/encrypt-adapter.ts
+++ src/context/auth-context/api-key/infrastructure/encrypt-adapter.ts
@@ -30,7 +30,7 @@ export class EncryptAdapter implements ApiKeyEncryptPrivateKey {
     const ENCRYPTION_KEY =
       this.configService.get<string>('ENCRYPTION_KEY') ||
       '0f0dd60415efd0a1d5c4409ed92fc1df3e4cfc517c4d3ad7d1e1d828f45f2bd4';
-    this.logger.log('encrypted', encrypted);
+    this.logger.log(`encrypted: ${encrypted}`);
     const [ivHex, encryptedData] = encrypted.split(':');
     this.logger.log(`ivHex: ${ivHex}`);
     this.logger.log(`encryptedData: ${encryptedData}`);
