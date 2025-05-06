85c8babe07c6f652827be31821cba8e5a9b147c1
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-05-06 16:24:29 +0200

    refactor: corregir nombres de archivo en importaciones de TrackingEvent
    Actualiza las importaciones de TrackingEventTypeOrmEntity para reflejar el nombre correcto del archivo, mejorando la claridad y consistencia del código.

--- src/context/tracking/tracking.module.ts
+++ src/context/tracking/tracking.module.ts
@@ -3,7 +3,7 @@ import { CreateTrackingEventCommandHandler } from './application/commands/create
 import { TRACKING_EVENT_REPOSITORY } from './domain/tracking-event.repository';
 import { TypeOrmTrackingEventAdapter } from './infrastructure/persistence/type-orm-tracking-event.adapter';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TrackingEventTypeOrmEntity } from './infrastructure/persistence/tracking-event.typeorm-entity';
+import { TrackingEventTypeOrmEntity } from './infrastructure/persistence/tracking-event.typeorm.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TrackingEventTypeOrmEntity])],
