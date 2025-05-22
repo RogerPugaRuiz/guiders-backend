645c13ca93aa1d0cd8293a408913163c6572c07f
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-05-22 20:58:36 +0200

    feat: agregar migración automática para la tabla visitor_intent
    Se añaden nuevas columnas: tags, priceRange, navigationPath y description a la tabla visitor_intent.

---
+++ src/migrations/1747940290178-auto-migration-2025-05-22.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AutoMigration202505221747940290178 implements MigrationInterface {
+    name = 'AutoMigration202505221747940290178'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "visitor_intent" ADD "tags" jsonb`);
+        await queryRunner.query(`ALTER TABLE "visitor_intent" ADD "priceRange" jsonb`);
+        await queryRunner.query(`ALTER TABLE "visitor_intent" ADD "navigationPath" jsonb`);
+        await queryRunner.query(`ALTER TABLE "visitor_intent" ADD "description" character varying(512)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "visitor_intent" DROP COLUMN "description"`);
+        await queryRunner.query(`ALTER TABLE "visitor_intent" DROP COLUMN "navigationPath"`);
+        await queryRunner.query(`ALTER TABLE "visitor_intent" DROP COLUMN "priceRange"`);
+        await queryRunner.query(`ALTER TABLE "visitor_intent" DROP COLUMN "tags"`);
+    }
+
+}
