78bd2e83a284f7113bad0e5e290405fe0831ada2
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-06-19 09:30:34 +0200

    test: agregar pruebas de integración para RedisConnectionService Implementar pruebas para verificar las operaciones de guardar, encontrar y eliminar usuarios en el servicio de conexión Redis, asegurando la limpieza de datos después de cada prueba.

--- src/context/real-time/infrastructure/__tests__/redis-connection.service.int-spec.ts
+++ src/context/real-time/infrastructure/__tests__/redis-connection.service.int-spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisConnectionService } from '../redis-connection.service';
 import { ConnectionUser } from '../../domain/connection-user';
