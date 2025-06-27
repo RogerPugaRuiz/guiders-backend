e5775b6adb143ad7fc0223dbe22b305d58fac1ee
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-06-27 11:17:56 +0200

    refactor: eliminar el parámetro companyId en la obtención de comerciales conectados
    Se simplifica la llamada a getConnectedCommercials al eliminar el uso del companyId, manteniendo la funcionalidad intacta.

--- src/context/real-time/application/event/recalculate-assignment-on-commercial-connected.event-handler.ts
+++ src/context/real-time/application/event/recalculate-assignment-on-commercial-connected.event-handler.ts
@@ -67,9 +67,7 @@ export class RecalculateAssignmentOnCommercialConnectedEventHandler
     }
     // Obtenemos los comerciales conectados
     const connectedCommercials =
-      await this.commercialAssignmentService.getConnectedCommercials(
-        event.connection.companyId,
-      );
+      await this.commercialAssignmentService.getConnectedCommercials();
 
     // Si no hay comerciales conectados, no hay nada que hacer
     if (connectedCommercials.length === 0) {
