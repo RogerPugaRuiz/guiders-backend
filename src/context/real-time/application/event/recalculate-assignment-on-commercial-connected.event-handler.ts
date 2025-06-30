853e1d2421207416d77a957c100633c4fdb045ee
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-06-30 13:31:38 +0200

    refactor: actualizar la obtención de comerciales conectados por compañía
    Se modifica la llamada a la función para obtener los comerciales conectados,
    especificando la compañía del comercial conectado.

--- src/context/real-time/application/event/recalculate-assignment-on-commercial-connected.event-handler.ts
+++ src/context/real-time/application/event/recalculate-assignment-on-commercial-connected.event-handler.ts
@@ -77,9 +77,11 @@ export class RecalculateAssignmentOnCommercialConnectedEventHandler
       );
       return;
     }
-    // Obtenemos los comerciales conectados
+    // Obtenemos los comerciales conectados de la misma compañía
     const connectedCommercials =
-      await this.commercialAssignmentService.getConnectedCommercials();
+      await this.commercialAssignmentService.getConnectedCommercials(
+        event.connection.companyId,
+      );
 
     // Si no hay comerciales conectados, no hay nada que hacer
     if (connectedCommercials.length === 0) {
